f066e6877fbd6b5f54b941b634ea57e0a2303486
maureen-qui <maureenwangui08@yahoo.com>
2023-02-07 19:59:49 -0800

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,11 +4,11 @@
 
 def write_file(prmFileName="", prmText=""):
     """
-      function that write a specific text in a specific file
+        function that write a specific text in a specific file
 
-      Args:
-          prmFileName: name of the file
-          prmText: text to write
+        Args:
+            prmFileName: name of the file
+            prmText: text to write
      """
      nbCharacter = 0
      with open(prmFileName, 'w', encoding="UTF-8") as file:
